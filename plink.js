1a8554f4edda3f15755ac86a00e5e377a5b0dd6b
MTNB <@CS>
2015-10-28 19:41:12 +0800

    plink.js modified again

--- plink.js
+++ plink.js
@@ -1,7 +1,18 @@
 var audioContext = new AudioContext();
 
+var minFrequency = 50;
+var maxFrequency = 2000;
+
 var oscillator = audioContext.createOscillator();
 oscillator.connect(audioContext.destination);
-oscillator.frequency.value = 1000;
+oscillator.frequency.value = 5000;
 oscillator.start(audioContext.currentTime);
 
+document.addEventListener('mousemove', function(e) {
+	var percentage = 1 - e.pageY/window.innerHeight;
+	var frequency = minFrequency + (maxFrequency - minFrequency) * percentage;
+	console.log(frequency);
+
+	oscillator.frequency.linearRampToValueAtTime(frequency, audioContext.currentTime);
+
+});
